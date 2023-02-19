2ce45fd5adf13d25549a63ed94e6226d18c71cd0
Vishvam10 <s.vishvam1025@gmail.com>
2023-02-20 02:03:38 +0530

    Added basic add label functionality

--- chrome_extension/scripts/contentScript.js
+++ chrome_extension/scripts/contentScript.js
@@ -10,24 +10,52 @@
 
 
 function remark_init() {
-    // registerScripts();
-    startAnnotationProcess();
-       
+    startAnnotationProcess();  
 }
 
 
+
 function startAnnotationProcess() {
+
+    let annotations = []
     
-    createCSSClass(".highlight_element_light", "outline: solid 1px #ff28009c; border-radius: 0.4rem; padding: 0.4rem; cursor: crosshair;")
+    createCSSClass(".remark_highlight_element_light", "outline: dashed 1px #ff28009c; border-radius: 0.4rem; padding: 0.4rem; cursor: crosshair;")
+    createCSSClass(".remark_highlight_element_strong", "outline: solid 1px #ff28009c; border-radius: 0.4rem; padding: 0.4rem; cursor: crosshair;")
     
     document.body.addEventListener("click", (e) => {
         e.preventDefault();
         e.stopPropagation();
         console.log("clicked : ");
+
+        // Add label
+
+        const t = e.target;
+        if(t.classList.contains("remark_highlight_element_light")) {
+            if(t.classList.contains("remark_highlight_element_strong")) {
+                return;
+            }
+            const rect = t.getBoundingClientRect();;
+            const x = Math.round(rect.x, 2), y = Math.round(rect.y, 2), w = Math.round(rect.width, 2), h = Math.round(rect.height, 2);
+            console.log(x,y,w,h);
+
+            annotations.push({
+                "id" : Math.random() * 10000,
+                "type" : t.tagName.toLowerCase(),
+                "coordinates" : [x, y, w, h],
+                "text" : t.innerText,
+                "parent" : t.parentNode.tagName.toLocaleLowerCase()
+            })
+
+            console.log("annotations : ", annotations);
+
+            t.classList.remove("remark_highlight_element_light");
+            t.classList.add("remark_highlight_element_strong");
+        }
+
     });
     
-    document.body.addEventListener("mouseover", mouseOverListener)
-    document.body.addEventListener("mouseout", mouseOutListener)
+    document.body.addEventListener("mouseover", mouseOverListener);
+    document.body.addEventListener("mouseout", mouseOutListener);
     
 }
 
@@ -50,7 +78,7 @@ function mouseOverListener(e) {
     const tag = e.target.tagName;
     if (VALID_HTML_ELEMENTS.includes(tag)) {
         const targetHTMLElement = e.target;
-        targetHTMLElement.classList.toggle("highlight_element_light");
+        targetHTMLElement.classList.toggle("remark_highlight_element_light");
     }
 }
 
@@ -63,6 +91,6 @@ function mouseOutListener(e) {
     const tag = e.target.tagName;
     if (VALID_HTML_ELEMENTS.includes(tag)) {
         const targetHTMLElement = e.target;
-        targetHTMLElement.classList.toggle("highlight_element_light");
+        targetHTMLElement.classList.toggle("remark_highlight_element_light");
     }
 }
