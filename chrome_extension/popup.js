d6569776569f3ae64fd022cc2a844ee7a3df7598
Vishvam10 <s.vishvam1025@gmail.com>
2023-03-03 22:51:07 +0530

    File renaming

--- chrome_extension/popup.js
+++ chrome_extension/popup.js
@@ -1,87 +1,137 @@
-const app = document.getElementById("root");
-const login = document.getElementById("login");
-const loginForm = document.getElementById("login-form");
+window.onload = async function () {
 
-onload = async function () {
-  const isRegistered = await getDataFromStorage("email");
-  if (
-    typeof isRegistered === "object" &&
-    Object.keys(isRegistered).length === 0
-  ) {
-    app.style.display = "none";
-    login.style.display = "block";
-
-    loginForm.addEventListener("submit", async (event) => {
-      event.preventDefault();
-      const data = new FormData(event.target);
-      const email = data.get("email");
-      await setDataToStorage("email", email);
-      app.style.display = "block";
-      login.style.display = "none";
-    });
-  }
-};
-
-
-document.getElementById("remark_start").addEventListener("click", (e) => {
-    e.preventDefault();
-    e.stopPropagation();
-    if(e.target.innerText == "Start Annotation") {
-        document.querySelector(".remark_init_container").classList.add("remark_init_container_resize");
-        e.target.innerText = "Stop Annotation";
-
-        renderMenu();
-        
-        removeAllExistingModals();
-        attachListeners()
-        startAnnotationProcess();
-    }
-    else if(e.target.innerText == "Stop Annotation") {
-        e.target.innerText = "Start Annotation";
-        stopAnnotationProcess();
-        document.querySelector(".remark_init_container").classList.remove("remark_init_container_resize");
-        return;
-    } else {
-        return;
-    }
-})
-
-const screenShotBtn = document.getElementById("remarkTakeScreenShot");
-screenShotBtn.addEventListener("click", async (e) => {
-    e.preventDefault();
-    e.stopPropagation();
-
-    const tab = await getCurrentTab();
-    const labels = await getLabels(tab);
-  
-    const labelFile = new File([labels], 'label.txt', { type: 'text/plain' })
-    //DISABLE: LABELING CSS WHILE SCREENSHOT
-    // await chrome.scripting.executeScript({
-    //   target: { tabId: tab.id },
-    //   func: () => {
-    //     const style = document.createElement('style');
-    //     style.setAttribute('id', 'remark-styles');
-    //     style.innerHTML = `
-    //       [data-remark-annotation]:after { display: none; }
-    //     `
-    //     document.head.appendChild(style);
-    //   }
-    // })
-    const dataSrc = await Screenshot(tab);
+    const Screenshot = async function (tab) {
+        const windowId = tab.windowId;
+        return new Promise((res) => chrome.windows.get(windowId, { populate: true }, async function (window) {
+          const width = window.tabs[0].width;
+          const height = window.tabs[0].height;
+      
+          console.log("window", width, height);
+          const [{ result }] = await chrome.scripting.executeScript({ 
+            target: { tabId: tab.id }, 
+            func: () => {
+              return document.body.scrollHeight;
+            }
+          });
+      
+          const canvas = document.createElement("canvas");
+          canvas.height = 0;
+          const context = canvas.getContext("2d");
+          const times = Math.ceil(result / height);
+          const Sleep = (n) => new Promise((res, rej) => setInterval(res, n))
+          const screenShots = [];
+          for(let i = 0, top = 0; i < times; i++, top += height) {
+            await chrome.scripting.executeScript({ 
+              target: { tabId: tab.id }, 
+              func: (top) => {
+                console.log('scrolltop', top);
+                document.documentElement.scrollTop = top;
+              },
+              args: [top]
+            });
+      
+            await Sleep(550);
+            await new Promise((res, rej) => {
+              chrome.tabs.captureVisibleTab(
+                windowId,
+                { format: "png" },
+                function (dataUrl) {
+                  screenShots.push(dataUrl);
+                  return res(true);
+                }
+              );
+            })
+          }
+          const getDataImageDIM = async (src) => {
+            const img = new Image();
+            img.src = src;
+            return new Promise((res) => img.onload = () => {
+              res([img.width, img.height]);
+            });
+          }
+      
+          const [screenshotWidth, screenshotHeight] = await getDataImageDIM(screenShots[0]);
+          const canvasHeight = (screenshotHeight * result) / height;
+      
+          canvas.height = canvasHeight;
+          canvas.width = screenshotWidth;
+      
+          for(let i = 0, top = 0; i < screenShots.length; i++, top += screenshotHeight) {
+            const img = document.createElement("img");
+            img.src = screenShots[i];
+      
+            if(i === screenShots.length - 1) top = canvasHeight - screenshotHeight;
+      
+            await new Promise((res) => {
+                img.onload = () => {
+                  context.drawImage(img, 0, top);
+                  res(true);
+                }
+            })
+          }
+          return res(canvas.toDataURL('image/png'));
+        }));
+    };
     
-    // await chrome.scripting.executeScript({
-    //   target: { tabId: tab.id },
-    //   func: () => {
-    //     const style = document.getElementById('remark-styles');
-    //     style.parentElement.removeChild(style);
-    //   }
-    // })
-    console.log('labels', labelFile, dataSrc);
+    document.getElementById("remark_start").addEventListener("click", async(e) => {
+        e.preventDefault();
+        e.stopPropagation();
+        const started = await getDataFromStorage("remarkStarted");
+        if(started === true) {
+            e.target.innerText == "Stop Annotation"
+        } else {
+            document.querySelector(".remark_init_container").classList.add("remark_init_container_resize");
+            e.target.innerText = "Stop Annotation";
+            const tab = await getCurrentTab();
+            chrome.scripting.executeScript({
+                target: { tabId: tab.id },
+                files: ["/scripts/injectedScript.js"],
+            });
+        }
+    })
 
-})
+    const screenShotBtn = document.getElementById("remarkTakeScreenShot");
+    screenShotBtn.addEventListener("click", async (e) => {
+        e.preventDefault();
+        e.stopPropagation();
+    
+        const tab = await getCurrentTab();
+        const dataURI = await Screenshot(tab);
+        console.log(dataURI);
+        
+        chrome.tabs.sendMessage(tab.id,{
+          message: dataURI
+        });
+    })
+}
 
-function sendMessage(message) {
-chrome.runtime.sendMessage(message, (response) => {
-    console.log("received  data", response);
-});
+
+
+async function getCurrentTab() {
+    let queryOptions = { active: true };
+    let [tab] = await chrome.tabs.query(queryOptions);
+    return tab;
+}
+
+
+function setDataToStorage(key, value) {
+    try {
+        // [k] is a computed property. 
+        // Without it, we can not set dynamic keys.
+        chrome.storage.local.set({
+            [key]: value 
+        });
+    } catch(e) {
+        console.log("chrome error : ", e.message)
+    }
+}
+
+function getDataFromStorage(key) {
+    console.log("reached : ", key)
+    return new Promise((resolve) => {
+            chrome.storage.local.get([key], function(res) {
+                resolve(res);
+            })
+        }
+    )
 }
--- chrome_extension/scripts/backgroundScript.js
+++ chrome_extension/scripts/backgroundScript.js
@@ -1,35 +1,10 @@
 console.log("from background", chrome.tabs);
 
-chrome.action.onClicked.addListener(function (tab) {
-  console.log("CLICKED EXTENSION ICON");
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    files: ["/scripts/contentScript.js"],
-  });
-});
+// chrome.action.onClicked.addListener(function (tab) {
+//   console.log("CLICKED EXTENSION ICON");
+//   chrome.scripting.executeScript({
+//     target: { tabId: tab.id },
+//     files: ["/scripts/contentScript.js"],
+//   });
+// });
 
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message === "takeScreenshot") {
-    chrome.tabs.captureVisibleTab(null,{},function(dataUri){
-        console.log(dataUri);
-        sendData(dataUri)
-    });
-    }
-});
-
-function sendMessage(message) {
-    chrome.runtime.sendMessage(message, (response) => {
-        console.log("bg received  data", response);
-    });
-}
-
-async function getCurrentTab() {
-  let queryOptions = { active: true };
-  let [tab] = await chrome.tabs.query(queryOptions);
-  return tab;
-}
-
-async function getCurrentWindow() {
-  const w = await chrome.windows.getCurrent({ populate: true });
-  return w;
-}
